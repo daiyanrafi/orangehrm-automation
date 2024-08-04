103e6b5081460eb4d7ebefec37e412abc259499c
pifflox <daiyanrafi1@gmail.com>
2024-08-04 17:31:24 +0600

    add

--- e2e/suites/openHRMLoginAndNavigate.spec.ts
+++ e2e/suites/openHRMLoginAndNavigate.spec.ts
@@ -1,4 +1,4 @@
 import { test } from '@playwright/test'
 import openHRMLoginAndNavigateTests from '../testCases/openHRMLoginAndNavigate.spec'
 
-test.describe('Open HRM Login and Navigate Test Suite', openHRMLoginAndNavigateTests)
+// test.describe('Open HRM Login and Navigate Test Suite', openHRMLoginAndNavigateTests)
